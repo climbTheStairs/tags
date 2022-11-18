473e826aa0b84c3a21dec6e3c785d8301e5cece2
climbTheStairs <yxtqwf+github@gmail.com>
2022-11-18 15:04:47 -0800

    operations.py: minor changes
    replace tuples with dicts in OPS, use Stack.peek() instead of redundant Stack.push(Stack.pop())

--- operations.py
+++ operations.py
@@ -5,21 +5,21 @@ from stack import Stack
 # Operations are in reverse
 # because items are popped in reverse
 MATH_OPS = {
-    "+": (2, lambda x, y : y+x),
-    "-": (2, lambda x, y : y-x),
-    "*": (2, lambda x, y : y*x),
-    "/": (2, lambda x, y : y/x),
-    "%": (2, lambda x, y : y%x),
+    "+": { "arity": 2, "op": lambda x, y : y+x },
+    "-": { "arity": 2, "op": lambda x, y : y-x },
+    "*": { "arity": 2, "op": lambda x, y : y*x },
+    "/": { "arity": 2, "op": lambda x, y : y/x },
+    "%": { "arity": 2, "op": lambda x, y : y%x },
 }
 BOOL_OPS = {
-    "!": (1, lambda x : not x),
-    "&": (2, lambda x, y : y and x),
-    "|": (2, lambda x, y : y or x),
+    "!": { "arity": 1, "op": lambda x : not x },
+    "&": { "arity": 2, "op": lambda x, y : y and x },
+    "|": { "arity": 2, "op": lambda x, y : y or x },
 }
 BOOL_OPS_PY = {
-    "not": (1, lambda x : not x),
-    "and": (2, lambda x, y : y and x),
-    "or":  (2, lambda x, y : y or x),
+    "not": { "arity": 1, "op": lambda x : not x },
+    "and": { "arity": 2, "op": lambda x, y : y and x },
+    "or":  { "arity": 2, "op": lambda x, y : y or x },
 }
 
 def main():
@@ -64,23 +64,23 @@ def infix_to_rpn(tokens, ops):
     using Dijkstra's shunting yard algorithm;
     see <https://en.wikipedia.org/wiki/Shunting_yard_algorithm?oldid=1102884909#The_algorithm_in_detail>
     tokens: iterable<str>
-    ops: dict<str, function>
+    ops: dict<str, dict<"arity": int, "op": function>>
     return: list<str>, ValueError
     """
     rpn = []
     ops_stk = Stack() # operators stack
     for t in tokens:
-        if t in ops and ops[t][0]==1:
+        if t in ops and ops[t]["arity"]==1:
             ops_stk.push(t)
             continue
-        if t in ops and ops[t][0]==2:
+        if t in ops and ops[t]["arity"]==2:
             while True:
-                op, e = ops_stk.pop()
+                op, e = ops_stk.peek()
                 if type(e) == IndexError:
                     break
                 if op == "(":
-                    ops_stk.push(op)
                     break
+                op, _ = ops_stk.pop()
                 rpn.append(op)
             ops_stk.push(t)
             continue
@@ -110,7 +110,7 @@ def eval_rpn(rpn, ops, f = lambda x : x):
     """
     evaluate expression in RPN
     rpn: iterable<str>
-    ops: dict<str, function>
+    ops: dict<str, dict<"arity": int, "op": function>>
     f: function, apply f to each non-operator token
     return: object, ValueError
     """
@@ -120,13 +120,13 @@ def eval_rpn(rpn, ops, f = lambda x : x):
     for t in rpn:
         if t in ops:
             args = []
-            for _ in range(ops[t][0]):
+            for _ in range(ops[t]["arity"]):
                 arg, e = stk.pop()
                 if type(e) == IndexError:
                     return None, ValueError("eval_rpn: " \
                         f"missing operand(s) for operation \"{t}\"")
                 args.append(arg)
-            stk.push(ops[t][1](*args))
+            stk.push(ops[t]["op"](*args))
             continue
         stk.push(f(t))
     out, _ = stk.pop() # stk cannot be possibly empty here
